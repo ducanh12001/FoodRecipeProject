1e1ab702d1e8653ae2fb72e1cd77082c747c1683
ducanh12001 <70888594+ducanh12001@users.noreply.github.com>
2022-09-27 23:07:31 +0700

    list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import FoodList from './components/FoodList/FoodList';
 
 function App() {
   return (
     <div className="App">
-      b
+      <FoodList />
     </div>
   );
 }
