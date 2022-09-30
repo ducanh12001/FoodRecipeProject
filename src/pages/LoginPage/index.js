f90f05fae15d629dbeb56d7a8066fb2d4bfb8715
LongHr2001 <mitom2007@gmail.com>
2022-10-01 00:06:16 +0700

    LoginPage
    + Tweaked login page layout
    + Added basic API call (not finished yet)

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,41 +1,97 @@
 import React, { useState } from "react";
 import { Button, Checkbox, Form, Input, Card } from 'antd';
 import { GoogleOutlined } from '@ant-design/icons';
+import axios from "axios";
+
+import "./style.css";
 
 function LoginPage() {
+	const [userInfo, setUserInfo] = useState({
+		email: "",
+		password: ""
+	});
+	
+	const login = () => {
+		console.log(userInfo);
+		
+		axios.post('http://13.215.176.20:3000/api/auth/login', userInfo).then((res) => console.log(res));
+		//if res === 200, redirect
+	}
+	
+	
 	return (
-	<div style={{}}>
-		<div style={{ backgroundImage: "url(/login_wallpaper.png)", display: "flex", justifyContent: "center", height: "100%", backgroundSize: "cover"}}>
-			<Card style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
-				<Form>
-					<div>
-						<h1><b>FoodRecipeProject</b></h1>
-					</div>
-					
-					<Form.Item label="Email" rules={[{required: true, message: 'Email is required'}]}>
-						<Input />
-					</Form.Item>
-					
-					<Form.Item label="Password" rules={[{required: true, message: 'Password is required'}]}>
-						<Input.Password />
-					</Form.Item>
-					
-					<Form.Item>
-						<Button type="primary" htmlType="submit" style={{display: "flex", marginLeft: "auto", marginRight: "auto", alignItems: "center"}}>
-							Login
-						</Button>
-					</Form.Item>
-					
-					<Form.Item>
-						<Button style={{display: "flex", marginLeft: "auto", marginRight: "auto", alignItems: "center"}}>
-							<GoogleOutlined />
-							Login with Google
-						</Button>
-					</Form.Item>
-				</Form>
-			</Card>
+	<div>
+		<div
+		className="background_image"
+		style={{backgroundImage: "url(/login_wallpaper.png)", flexDirection: 'column'}}
+		>
+			<div style={{flex: 1}}></div>
+			
+			<div style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
+				<div style={{flex: 1}}></div>
+				
+				<Card style={{ alignItems: 'center', justifyContent: 'center', borderRadius: 20}}>
+					<Form layout="vertical">
+						<div style={{flex: 1, textAlign: 'center'}}>
+							<h1><b>FoodRecipeProject</b></h1>
+						</div>
+						
+						<Form.Item
+						name="email"
+						label="Email"
+						rules={[{required: true, message: 'Email is required'}]}
+						style={{flex: 1}}
+						>
+							<Input
+							onChange={(e) => setUserInfo({...userInfo, email: e.target.value})}
+							/>
+						</Form.Item>
+						
+						<Form.Item
+						name="password"
+						label="Password"
+						rules={[{required: true, message: 'Password is required'}]}
+						style={{flex: 1}}
+						>
+							<Input.Password
+							onChange={(e) => setUserInfo({...userInfo, password: e.target.value})}
+							/>
+						</Form.Item>
+						
+						<div style={{flex: 1}}>
+							<Form.Item>
+								<Button
+								type="primary"
+								htmlType="submit"
+								style={{display: "flex", marginLeft: "auto", marginRight: "auto", alignItems: "center"}}
+								onClick={login}
+								>
+									Login
+								</Button>
+							</Form.Item>
+							
+							<Form.Item>
+								<Button
+								style={{display: "flex", marginLeft: "auto", marginRight: "auto", alignItems: "center"}}
+								>
+									<GoogleOutlined />
+									Login with Google
+								</Button>
+							</Form.Item>
+						</div>
+						
+						<div style={{flex: 1, textAlign: 'center'}}>
+							<u>Sign up</u>
+						</div>
+					</Form>
+				</Card>
+				
+				<div style={{flex: 1}}></div>
+			</div>
+			
+			<div style={{flex: 1}}></div>
 		</div>
-		</div>
+	</div>
 	);
 }
 
