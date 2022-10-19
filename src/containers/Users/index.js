c7480efccbf94228ee97c6598d5cce46ff76e7da
LongHr2001 <mitom2007@gmail.com>
2022-10-19 22:55:10 +0700

    Basic user profile

--- src/containers/Users/index.js
+++ src/containers/Users/index.js
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
+import { Menu } from "antd";
 
-//import "./style.css";
+import "./style.css";
 import RecipeCard from "../../components/RecipeCard";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+
+function getItem(label, key, icon, children, type) {
+	return {
+	key,
+	icon,
+	children,
+	label,
+	type,
+	};
+}
+
+const items = [
+	getItem('Your posts', '1'),
+	getItem('Bookmarked posts', '2'),
+	getItem('Personal Info', '3'),
+];
 
 function Users() {
 	
@@ -11,8 +28,18 @@ function Users() {
 	<div>
 		<Header />
 		
-		<div>
-		
+		<div className='profile-page'>
+			<div style={{flex: 1}}>
+				<Menu
+				defaultSelectedKeys={['1']}
+				mode="inline"
+				items={items}
+				/>
+			</div>
+			
+			<div style={{flex: 3}}>
+				Profile content
+			</div>
 		</div>
 		
 		<Footer />
